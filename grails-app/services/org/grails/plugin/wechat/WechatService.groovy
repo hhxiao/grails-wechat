0ae68cdb41f5dfc273f8d54688ae2d2d79faefd5
unknown <haihxiao@HAIHXIAO-WS02.cisco.com>
2014-09-29 21:47:14 +0800

    wechat basic service

--- grails-app/services/org/grails/plugin/wechat/WechatService.groovy
+++ grails-app/services/org/grails/plugin/wechat/WechatService.groovy
@@ -1,17 +1,24 @@
 package org.grails.plugin.wechat
 
 import org.grails.plugin.wechat.message.Message
+import org.grails.plugin.wechat.message.ResponseMessage
+import org.grails.plugin.wechat.token.AccessToken
+import org.grails.plugin.wechat.util.HttpUtils
+import org.grails.plugin.wechat.util.JsonHelper
+import org.grails.plugin.wechat.util.MessageUtils
 import org.springframework.beans.factory.InitializingBean
 
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
 /**
+ * 微信基本接口
+ *
  * Created by haihxiao on 17/9/14.
  */
 class WechatService {
-    private static final String CUSTOM_SERVICE_URL = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=ACCESS_TOKEN"
-
-    def wechatConfigService
-
-    void sendCustomMessage(Message message) {
-
+    void sendReply(HttpServletResponse response, ResponseMessage reply) {
+        response.setContentType('application/xml')
+        response.sendError(HttpURLConnection.HTTP_OK, MessageUtils.toXml(reply))
     }
 }
