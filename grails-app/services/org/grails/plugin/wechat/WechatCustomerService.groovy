01eadae714c8865f25d236021136ea05fd6b018e
unknown <haihxiao@HAIHXIAO-WS02.cisco.com>
2014-09-29 21:46:41 +0800

    wechat customer service

---
+++ grails-app/services/org/grails/plugin/wechat/WechatCustomerService.groovy
@@ -0,0 +1,22 @@
+package org.grails.plugin.wechat
+
+import org.grails.plugin.wechat.message.ResponseMessage
+import org.grails.plugin.wechat.util.HttpUtils
+import org.grails.plugin.wechat.util.MessageUtils
+
+/**
+ * 微信客服接口
+ *
+ * Created by haihxiao on 2014/9/29.
+ */
+class WechatCustomerService {
+    private static final String CUSTOM_SERVICE_URL = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=ACCESS_TOKEN"
+
+    def wechatTokenService
+
+    String sendMessage(ResponseMessage message) {
+        String accessToken = wechatTokenService.accessToken
+        String url = CUSTOM_SERVICE_URL.replace('ACCESS_TOKEN', accessToken)
+        HttpUtils.postJson(url, MessageUtils.toJson(message))
+    }
+}
