c2ed0d03ca14184c93549be692f8078330b66f8e
unknown <haihxiao@HAIHXIAO-WS02.cisco.com>
2014-09-29 21:42:54 +0800

    access token retrieval

--- grails-app/services/org/grails/plugin/wechat/WechatConfigService.groovy
+++ grails-app/services/org/grails/plugin/wechat/WechatConfigService.groovy
@@ -1,6 +1,8 @@
 package org.grails.plugin.wechat
 
 import org.grails.plugin.wechat.token.AccessToken
+import org.grails.plugin.wechat.util.HttpUtils
+import org.grails.plugin.wechat.util.JsonHelper
 import org.springframework.beans.factory.InitializingBean
 
 import java.util.concurrent.TimeUnit
@@ -9,14 +11,20 @@ import java.util.concurrent.TimeUnit
  * Created by hhxiao on 2014/9/29.
  */
 class WechatConfigService implements InitializingBean {
+    private static final String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APP_ID&secret=APP_SECRET"
+
     def grailsApplication
 
     String appId
     String appSecret
     String appToken
 
-    String getAccessToken() {
-        return AccessToken.get(appId, appSecret).accessToken
+    String accessToken
+
+    private AccessToken get(String appId, String appSecret) throws WeixinException {
+        String url = ACCESS_TOKEN_URL.replace('APP_ID', appId).replace('APP_SECRET', appSecret)
+        String text = HttpUtils.get(url)
+        return JsonHelper.parseJson(text, AccessToken.class)
     }
 
     @Override
@@ -27,14 +35,18 @@ class WechatConfigService implements InitializingBean {
         if(!appId || !appSecret || !appToken) {
             throw new IllegalStateException("Weixin AppId or AppSecret or AppToken is not configured")
         }
-        TimerTask task = new TimerTask() {
-            @Override
-            void run() {
-                // refresh token
-                //
-            }
-        }
-        Timer timer = new Timer()
-        timer.schedule(task, TimeUnit.SECONDS.toMillis(15), TimeUnit.SECONDS.toMillis(7200))
+//        TimerTask task = new TimerTask() {
+//            @Override
+//            void run() {
+//                // refresh token
+//                try {
+//                    accessToken = get(appId, appSecret).accessToken
+//                } catch(WeixinException we) {
+//                    println we.errcode + ":" + we.errmsg
+//                }
+//            }
+//        }
+//        Timer timer = new Timer()
+//        timer.schedule(task, TimeUnit.SECONDS.toMillis(15), TimeUnit.SECONDS.toMillis(7200))
     }
 }
--- src/groovy/org/grails/plugin/wechat/token/AccessToken.groovy
+++ src/groovy/org/grails/plugin/wechat/token/AccessToken.groovy
@@ -1,6 +1,7 @@
 package org.grails.plugin.wechat.token
 
 import org.grails.plugin.wechat.WeixinException
+import org.grails.plugin.wechat.util.HttpUtils
 import org.grails.plugin.wechat.util.JsonHelper
 
 /**
@@ -9,12 +10,4 @@ import org.grails.plugin.wechat.util.JsonHelper
 class AccessToken {
     String accessToken
     long expiresIn
-
-    static AccessToken get(String appId, String appSecret) throws WeixinException {
-        String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appId}&secret=${appSecret}"
-//        HttpsURLConnection urlConnection = (HttpsURLConnection)new URL(url).openConnection()
-//        String text = urlConnection.inputStream.text
-        String text = "{\"access_token\":\"xxx\",\"expires_in\":124}"
-        return JsonHelper.parseJson(text, AccessToken.class)
-    }
 }
