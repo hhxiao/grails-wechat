f909076704a29c3363cd8476c57cd9d0e31281d6
Haihua Xiao <hhxiao@gmail.com>
2014-12-05 22:46:35 +0800

    simple cache for access token

--- grails-app/services/org/grails/plugin/wechat/WechatTokenService.groovy
+++ grails-app/services/org/grails/plugin/wechat/WechatTokenService.groovy
@@ -5,11 +5,16 @@ import org.grails.plugin.wechat.util.HttpUtils
 import org.grails.plugin.wechat.util.JsonHelper
 import org.grails.plugin.wechat.util.SignatureHelper
 import org.springframework.beans.factory.InitializingBean
+
+import java.util.concurrent.TimeUnit
+
 /**
  * Created by hhxiao on 2014/9/29.
  */
 class WechatTokenService implements InitializingBean {
     private static final String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APP_ID&secret=APP_SECRET"
+
+    private static final int TOKEN_EXPIRATION_IN_MS = TimeUnit.MINUTES.toMillis(115)
 
     def grailsApplication
 
@@ -21,22 +26,43 @@ class WechatTokenService implements InitializingBean {
 
     synchronized AccessToken getAccessToken() throws WechatException {
         if(accessToken == null) {
-            String url = ACCESS_TOKEN_URL.replace('APP_ID', appId).replace('APP_SECRET', appSecret)
-            String text = HttpUtils.get(url)
-            accessToken = JsonHelper.parseJson(text, AccessToken.class)
+            // check cache first
+            boolean reload = true
+            int expiresInMs = TOKEN_EXPIRATION_IN_MS
+            File tokenFile = new File("wechat-token.dat")
+            if(tokenFile.file) {
+                long lastModified = tokenFile.lastModified()
+                long now = System.currentTimeMillis()
+                expiresInMs = TOKEN_EXPIRATION_IN_MS + lastModified - now
+
+                if(expiresInMs > 0) {
+                    // not expired yet
+                    reload = false
+                    String text = tokenFile.text
+                    accessToken = JsonHelper.parseJson(text, AccessToken.class)
+                    log.info"AccessToken loaded: ${accessToken.accessToken}"
+                }
+            }
+
+            if(reload) {
+                String url = ACCESS_TOKEN_URL.replace('APP_ID', appId).replace('APP_SECRET', appSecret)
+                String text = HttpUtils.get(url)
+                accessToken = JsonHelper.parseJson(text, AccessToken.class)
+                tokenFile.text = text
+                log.info"AccessToken retrieved: ${accessToken.accessToken}"
+            }
 
             // expire in about 2 hours
             Calendar cal = Calendar.getInstance()
-            cal.add(Calendar.MINUTE, 110)
+            cal.add(Calendar.MILLISECOND, expiresInMs)
             timer.schedule(new TimerTask() {
                 @Override
                 void run() {
                     accessToken = null
-                    log.info"AccessToken cleared"
+                    log.info"AccessToken expired"
                 }
             }, cal.time)
 
-            log.info"AccessToken retrieved: ${accessToken.accessToken}"
         }
         return accessToken
     }
