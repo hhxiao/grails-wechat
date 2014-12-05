18bfa7a61904a83f83ca5c4058cc6735d77cb784
Haihua Xiao <hhxiao@gmail.com>
2014-12-05 18:00:38 +0800

    make event type be upper case

--- src/groovy/org/grails/plugin/wechat/message/EventType.groovy
+++ src/groovy/org/grails/plugin/wechat/message/EventType.groovy
@@ -6,8 +6,8 @@ package org.grails.plugin.wechat.message
 public enum EventType {
     UNKNOWN,        // 未知事件
 
-    subscribe,      // 关注
-    unsubscribe,    // 取消关注
+    SUBSCRIBE,      // 关注
+    UNSUBSCRIBE,    // 取消关注
 
     SCAN,       // 扫描带场景值二维码
     LOCATION,   // 地理位置事件
@@ -15,20 +15,20 @@ public enum EventType {
     CLICK,  // 点击菜单拉取消息时的事件推送
     VIEW,   // 点击菜单跳转链接时的事件推送
 
-    scancode_push,      // 扫码推事件
-    scancode_waitmsg,   // 扫码推事件且弹出“消息接收中”提示框的事件推送
+    SCANCODE_PUSH,      // 扫码推事件
+    SCANCODE_WAITMSG,   // 扫码推事件且弹出“消息接收中”提示框的事件推送
 
-    pic_sysphoto,       // 弹出系统拍照发图的事件推送
-    pic_photo_or_album, // 弹出拍照或者相册发图的事件推送
-    pic_weixin,         // 弹出微信相册发图器的事件推送
-    location_select,    // 弹出地理位置选择器的事件推送
+    PIC_SYSPHOTO,       // 弹出系统拍照发图的事件推送
+    PIC_PHOTO_OR_ALBUM, // 弹出拍照或者相册发图的事件推送
+    PIC_WEIXIN,         // 弹出微信相册发图器的事件推送
+    LOCATION_SELECT,    // 弹出地理位置选择器的事件推送
 
     MASSSENDJOBFINISH,      // 消息群发结束
     TEMPLATESENDJOBFINISH;  // 模板消息发送结束
 
     static EventType of(String value) {
         try {
-            valueOf(value)
+            valueOf(value.toUpperCase())
         } catch(IllegalArgumentException e) {
             UNKNOWN
         }
