259ed82b7c327cb78828a5332fe9b70ccf7f743e
Haihua Xiao <hhxiao@gmail.com>
2015-01-10 20:30:07 +0800

    Update version number

--- WechatGrailsPlugin.groovy
+++ WechatGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import org.grails.plugin.wechat.util.SecurityHelper
 
 class WechatGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.0.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0.0 > *"
     // resources that are excluded from plugin packaging
