0f1825466327d1c74f33ae4d60f548c047c72042
Haihua Xiao <hhxiao@gmail.com>
2015-02-05 22:08:05 +0800

    implement user/group management for wechat

--- grails-app/services/org/grails/plugin/wechat/WechatUserService.groovy
+++ grails-app/services/org/grails/plugin/wechat/WechatUserService.groovy
@@ -10,28 +10,42 @@ import org.grails.plugin.wechat.util.JsonHelper
 @VerificationRequired
 class WechatUserService {
     private static final String USER_INFO_URL = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPEN_ID&lang=zh_CN'
+    private static final String GROUP_MEMBER_UPDATE_URL = 'https://api.weixin.qq.com/cgi-bin/groups/members/update?access_token=ACCESS_TOKEN'
+    private static final String REMARK_URL = 'https://api.weixin.qq.com/cgi-bin/user/info/updateremark?access_token=ACCESS_TOKEN'
 
     def wechatTokenService
 
-    WechatUser getUser(String openId) {
-        WechatUser user = WechatUser.findByOpenid(openId)
-        if(!user) {
-            String text = getUserInfo(openId)
-            user = JsonHelper.parseJson(text, WechatUser.class).save()
-        }
-        return user
+    void setUserRemark(String openId, String remark) {
+        AccessToken token = wechatTokenService.accessToken
+        String url = REMARK_URL.replace('ACCESS_TOKEN', token.accessToken)
+        Map map = new HashMap()
+        map.put('openid', openId)
+        map.put('remark', remark)
+        String result = HttpUtils.postJson(url, map)
+        log.info("Set user remark: ${openId} -> ${remark} - ${result}")
     }
 
-    void refreshUser(String openId) {
+    WechatUser getUser(String openId) {
         String text = getUserInfo(openId)
         WechatUser user = WechatUser.findByOpenid(openId)
         if(!user) {
-            JsonHelper.parseJson(text, WechatUser.class).save()
+            user = JsonHelper.parseJson(text, WechatUser.class).save()
         } else {
             WechatUser u = JsonHelper.parseJson(text, WechatUser.class)
             user.properties = u.properties
             user.save()
         }
+        return user
+    }
+
+    void updateGroup(String openId, int groupId) {
+        AccessToken token = wechatTokenService.accessToken
+        String url = GROUP_MEMBER_UPDATE_URL.replace('ACCESS_TOKEN', token.accessToken)
+        Map map = new HashMap()
+        map.put('openid', openId)
+        map.put('to_groupid', groupId)
+        String result = HttpUtils.postJson(url, map)
+        log.info("Updated group member: ${openId} -> ${groupId} - ${result}")
     }
 
     private String getUserInfo(String openId) {
