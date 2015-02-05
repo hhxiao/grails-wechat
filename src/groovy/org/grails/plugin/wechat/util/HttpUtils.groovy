8332786f4b7f525006cfd780c89859a37b8747ad
Haihua Xiao <hhxiao@gmail.com>
2015-02-05 22:00:49 +0800

    utf8 support

--- src/groovy/org/grails/plugin/wechat/util/HttpUtils.groovy
+++ src/groovy/org/grails/plugin/wechat/util/HttpUtils.groovy
@@ -1,10 +1,13 @@
 package org.grails.plugin.wechat.util
+
+import java.nio.charset.Charset
+
 /**
  * Created by haihxiao on 2014/9/29.
  */
 class HttpUtils {
 
-    public static final String CONTENT_TYPE_JSON = 'application/json'
+    public static final String CONTENT_TYPE_JSON = 'application/json; encoding=utf-8'
 
     static String get(String url) {
         new URL(url).getText('UTF-8')
@@ -23,7 +26,7 @@ class HttpUtils {
         conn.setRequestProperty("Content-Type", contentType)
         conn.setRequestMethod("POST")
         conn.setDoOutput(true)
-        OutputStreamWriter out = new OutputStreamWriter(conn.outputStream);
+        OutputStreamWriter out = new OutputStreamWriter(conn.outputStream, "UTF-8")
         out.write(content)
         out.flush()
         out.close()
